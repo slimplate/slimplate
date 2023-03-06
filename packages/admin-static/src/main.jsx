60a45e43deff09df3055f977cc77e28a15b17a1a
David Konsumer <konsumer@jetboystudio.com>
2023-03-06 14:41:50 -0800

    start of router

--- packages/admin-static/src/main.jsx
+++ packages/admin-static/src/main.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, useLocation } from 'wouter'
 
 import { SlimplateProvider, useSlimplate, AdminProjectList, AdminCollection, AdminContent, AdminEdit, widgets } from '@slimplate/react-flowbite-github'
 
@@ -9,31 +10,8 @@ import './index.css'
 // set this up in your .env file
 const { VITE_GITHUB_BACKEND, VITE_CORS_PROXY } = import.meta.env
 
-function App () {
-  const [selectedContent, setSelectedContent] = useState(false)
-  const [selectedCollection, setSelectedCollection] = useState(false)
-  const [selectedProject, setSelectedProject] = useState(false)
-  const { user } = useSlimplate()
-
-  const handleSelectedCollection = (collection, project) => {
-    setSelectedCollection(collection.name)
-    setSelectedContent(false)
-  }
-
-  const handleSelectedArticle = (article, collection, project) => {
-    setSelectedContent(article.filename)
-  }
-
-  const handleCreateArticle = () => {
-    console.log('CREATE ARTICLE')
-    setSelectedContent(false)
-    setSelectedContent(true)
-  }
-
-  return (
-    <div className='p-8'>
-      <UserMenu />
-      <div className='p-4' />
+/*
+<div className='p-4' />
       {!!user && (<AdminProjectList onSelect={p => setSelectedProject(p.full_name)} />)}
       {!user && (<div>Please login.</div>)}
 
@@ -71,6 +49,33 @@ function App () {
           />
         </>
       )}
+*/
+
+function PageDashboard () {
+  const [, navigate] = useLocation()
+  return (
+    <AdminProjectList onSelect={p => navigate(`/${p.full_name}/${p.branch.name}`)} />
+  )
+}
+
+function PageCollection ({ params: { username, project, branch } }) {
+  const [, navigate] = useLocation()
+  return (
+    <AdminCollection
+      onSelect={c => navigate(`/${username}/${project}/${branch}/${c.name}`)}
+      projectName={`${username}/${project}`}
+    />
+  )
+}
+
+function App () {
+  return (
+    <div className='p-8'>
+      <UserMenu />
+      <Route path='/' component={PageDashboard} />
+      <Route path='/:username/:project/:branch' component={PageCollection} />
+      <Route path='/:username/:project/:branch/:collection'>ADMIN CONTENT</Route>
+      <Route path='/:username/:project/:branch/:collection/:filename*'>ADMIN EDIT</Route>
     </div>
   )
 }
--- packages/admin-static/vite.config.js
+++ packages/admin-static/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { fileURLToPath } from 'url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
