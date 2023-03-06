fbc3a8de655c3637ee8fc8e6ab3a750359c3e8bb
David Konsumer <konsumer@jetboystudio.com>
2023-03-06 15:03:39 -0800

    single file build

--- packages/admin-static/vite.config.js
+++ packages/admin-static/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { fileURLToPath } from 'url'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   resolve: {
     alias: {
       '~': resolve(fileURLToPath(new URL('.', import.meta.url)), './src')
